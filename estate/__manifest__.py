e3c918cf4b3ec374d2a3b74a9d50c0e27cb564bc
Kenny <kenneth@filterplus.ee>
2025-09-16 11:58:36 +0300

    Update version number to 18.0.1.0.3 in __manifest__.py for the estate module.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,12 +1,12 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "18.0.1.0.2",  # Version
+    "version": "18.0.1.0.3",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     'data': [
     'security/ir.model.access.csv',
     ],
-    
+
     "installable": True,
     'license': 'LGPL-3',
 }
