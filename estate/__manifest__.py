61864b4587b2836e4c3134f28155fd4fbd95d05e
Kenny <kenneth@filterplus.ee>
2025-09-16 16:54:05 +0300

    Refactor estate property views by reintroducing the action window definition for property offers in estate_property_type_views.xml, ensuring improved accessibility and functionality in the user interface. Remove the previous definition from estate_property_offer_views.xml for better organization.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,14 +1,14 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "18.0.1.0.43",  # Version
+    "version": "18.0.1.0.44",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
         'views/estate_menus.xml',
     ],
 
