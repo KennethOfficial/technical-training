cbe9c14cd1472c408ebc4d27ad12902a6d990aca
Kenny <kenneth@filterplus.ee>
2025-09-16 12:07:30 +0300

    Update version number to 18.0.1.0.4 and add 'views/estate_property_views.xml' to the manifest data for the estate module.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,13 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "18.0.1.0.3",  # Version
+    "version": "18.0.1.0.4",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     'data': [
+
+    'views/estate_property_views.xml',
     'security/ir.model.access.csv',
+
     ],
 
     "installable": True,
