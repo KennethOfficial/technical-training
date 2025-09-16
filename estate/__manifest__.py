e6a74dec97d74ef275df2a817716a988a5fe9763
Kenny <kenneth@filterplus.ee>
2025-09-16 12:26:13 +0300

    Update version number to 18.0.1.0.8 in __manifest__.py and add 'views/estate_menus.xml' to the manifest data for the estate module.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "18.0.1.0.7",  # Version
+    "version": "18.0.1.0.8",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     'data': [
 
     'views/estate_property_views.xml',
+    'views/estate_menus.xml',
     'security/ir.model.access.csv',
 
     ],
